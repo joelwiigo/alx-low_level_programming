e29fd448a85328a193a9a7922ff2666873b6f12d
Lwiigo Joel <sjoelwiigo@gmail.com>
2022-07-05 06:19:57 -0700

    Task 4-is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  *Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
-int _isalpha(int c)
+int test_isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') ||
 		(c >= 'A' && c <= 'Z'))
