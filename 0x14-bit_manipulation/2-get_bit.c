3c94c47fbfb16381c8af6eb5f78f7a3d27badcce
Lwiigo Joel <sjoelwiigo@gmail.com>
2022-08-18 03:59:57 -0700

    Mandatory bit manipulation tasks

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,22 +1,21 @@
-#include <math.h>
 #include <stdio.h>
 #include "main.h"
-
+#include <math.h>
 /**
- * get_bit - A function that gets a bit at index
- * @n: The number to get bit from
- * @index: The index where the bit get at
- * Return: The value of the bit or -1 if an error occured
+ * get_bit - Write a function that returns the value of a bit at a
+ * given index.
+ * Prototype: int get_bit(unsigned long int n, unsigned int index);
+ * @index: is the index, starting from 0 of the bit you want to get
+ * @n: bit value in the index.
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
+	if (index > 63)
 		return (-1);
-	if ((n & max))
-		return (1);
-	else
-		return (0);
+	if (index == 0)
+	{
+		return (n & 1);
+	}
+	return (get_bit(n >> 1, index - 1));
 }
